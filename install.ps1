a22e246b12ce3ade545021f77fb47dabaafeb0ad
Dan <28414793+TsekNet@users.noreply.github.com>
2020-05-26 21:24:38 -0400

    Remove advanced function parameters.
    - These do not play well with PS v2/3

--- install.ps1
+++ install.ps1
@@ -1,48 +1,26 @@
-﻿<#
-.Synopsis
-    Install the TsekNet PowerShell profile.
+﻿###############################################################################
+# Install the TsekNet PowerShell profile from GitHub for the desired scope.
+#
+# By default, this will overwrite $PROFILE.CurrentUserCurrentHost.
+# See https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles
+# for more information. around scopes
+###############################################################################
 
-.DESCRIPTION
-    Install the TsekNet PowerShell profile from GitHub for the desired scope.
-    By default, the existing profile will not be overwritten.
+$scope = 'CurrentUserCurrentHost'
 
-.Parameter Scope
-    Name of the PowerShell profile scope to use.
-    See https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles for more information.
+# Profile.ps1 URL.
+$url = 'https://git.io/tsekprofile'
 
-.Parameter URL
-    Fully qualified URL to the profile.ps1 file to download.
-
-.Parameter Force
-    Whether to overwrite existing profiles.
-
-.EXAMPLE
-    Overwrite the AllUsersAllHosts PowerShell Profile with data downloaded from the URL.
-    .\install.ps1 -Scope 'AllUsersAllHosts' -URL 'http://example.com' -Force
-#>
-param(
-    [ValidateSet('AllUsersAllHosts', 'AllUsersCurrentHost', 'CurrentUserAllHosts', 'CurrentUserCurrentHost')]
-    [string]$Scope = "CurrentUserCurrentHost",
-    [Uri]$URL = 'https://git.io/tsekprofile',
-    [switch]$Force
-)
-
-$profile_dir = Split-Path $PROFILE.$Scope
-$profile_file = $profile.$Scope
-$request = Invoke-WebRequest $URL -UseBasicParsing  -ContentType "text/plain; charset=utf-8"
+$profile_dir = Split-Path $PROFILE.$scope
+$profile_file = $profile.$scope
+$request = Invoke-WebRequest $url -UseBasicParsing  -ContentType "text/plain; charset=utf-8"
 
 if (-not (Test-Path $profile_dir)) {
     New-Item -Path $profile_dir -ItemType Directory | Out-Null
     Write-Verbose "Created new profile directory: $profile_dir"
 }
 
-# Only overwrite existing profile if user specifies.
-if (-not $Force -and (Test-Path $profile_file)) {
-  Write-Warning "Profile: $profile_file detected, Use -Force to overwrite."
-}
-else {
-  # Leverage IO.File for PowerShell 5 comppatability.
-  [IO.File]::WriteAllLines($profile_file, $request.Content)
-  Write-Verbose "Wrote profile file: $profile_file with content from: $URL"
-  & $profile_file -Verbose
-}
+# Leverage IO.File for PowerShell 5 comppatability.
+[System.IO.File]::WriteAllLines($profile_file, $request.Content)
+Write-Verbose "Wrote profile file: $profile_file with content from: $url"
+& $profile_file -Verbose
